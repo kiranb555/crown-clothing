89598e586d5536a78287276ed1abed4f3b53e0ff
kiranb555 <kiran.basavaraj05@gmail.com>
2019-12-26 01:30:12 +0530

    update menu item with navigation withRouter Hoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component.jsx";
 
-const HomePage = () => {
+const HomePage = props => {
+  console.log(props);
   return (
     <div className="homepage">
       <Directory />
