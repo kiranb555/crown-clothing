cae61f785fc55c90b5c0354cda1e5cfc399201f0
kiranb555 <kiran.basavaraj05@gmail.com>
2020-01-01 01:35:28 +0530

    implemented firebase utils, included ability to store authenticated user to fire store

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 
-import { auth } from "./firebase/firebase.utils.js";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 
 class App extends Component {
   state = {
@@ -16,11 +16,25 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>
-      this.setState({
-        currentUser: user
-      })
-    );
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth
+        });
+      }
+    });
   }
   componentWillUnmount() {
     this.unsubscribeFromAuth();
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+
+import FormInput from "../form-input/form-input.component.jsx";
+import CustomButton from "../custom-button/custom-button.component.jsx";
+import {
+  auth,
+  createUserProfileDocument
+} from "../../firebase/firebase.utils.js";
+import "./sign-up.style.scss";
+
+class SignUp extends Component {
+  state = {
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  };
+
+  handleSumbit = async event => {
+    event.preventDefault();
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("Password don't match");
+      return;
+    }
+    try {
+      // this will take care of creating new user
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <div className="title">I do not have a account</div>
+        <span>Sign up with your email password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          ></FormInput>
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          ></FormInput>
+          <CustomButton type="submit" onClick={this.handleSumbit}>
+            Sign Up{" "}
+          </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
