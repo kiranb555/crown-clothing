2ff76a759f9f0d391c095f07662745969c00b41b
kiranb555 <kiran.basavaraj05@gmail.com>
2020-03-04 23:49:35 +0530

    removing redux logger from heroku master

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,11 @@ import { logger } from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
-const middleWares = [logger];
+const middleWares = [];
 
+if (process.env.NODE_ENV === "development") {
+  middleWares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export const persistor = persistStore(store);
